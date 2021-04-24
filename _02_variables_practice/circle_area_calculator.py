f7fb97a81c08549c50fd6573ca7077a0a90c78eb
root <root@root.com>
2021-04-24 00:03:18 +0000

    birthday

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -13,22 +13,31 @@ if __name__ == '__main__':
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
+    radius = simpledialog.askinteger('Title', "What do you want the radius to be in pixels?")
     
-    # Make a new turtle
+    # Make a new turtlem
+    thingy = turtle.Turtle()
     
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
+    thingy.circle(radius)
 
     # Call the turtle .penup() method
+    thingy.penup()
 
     # Move your turtle to a new x,y position using .goto()
+    thingy.goto(-6, 4)
 
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    area = math.pi*radius*radius
+
     # Write the area of your circle using the turtle .write() method
-    # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
+    thingy.write(arg="area = " + str(area), move=True, align='left', font=('Arial',10,'normal'))
 
     # Hide your turtle
+    thingy.hideturtle()
+
 
     # Call turtle.done()
+    turtle.done()
---
+++ _03_if_else/_1_unbirthday/Unbirthday.py
@@ -0,0 +1,5 @@
+from tkinter import simpledialog
+
+simpledialog.askstring('Title', "What is your birthday? (month and day)")
+
+
