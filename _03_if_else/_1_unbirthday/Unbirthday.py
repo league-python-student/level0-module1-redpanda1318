438d8f5f316face30345d648eb01be346550c5dc
root <root@root.com>
2021-05-01 00:03:32 +0000

    Unbirthday

--- _03_if_else/_1_unbirthday/Unbirthday.py
+++ _03_if_else/_1_unbirthday/Unbirthday.py
@@ -1,5 +1,18 @@
-from tkinter import simpledialog
+from tkinter import simpledialog, Tk, messagebox
 
-simpledialog.askstring('Title', "What is your birthday? (month and day)")
+window = Tk()
+window.withdraw()
+
+
+answer = simpledialog.askstring('Title', "What is your birthday? (month and day)")
+
+if answer == "04/30":
+    messagebox.showinfo('Title', "Happy Birthday")
+else:
+    messagebox.showinfo('Title', "Happy Unbirthday")
+
+
+
+
 
 
