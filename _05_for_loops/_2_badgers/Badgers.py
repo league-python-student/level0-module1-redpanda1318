f46974aecfa6b12705d3b8af33b0559f25c48deb
caris <yeahredpanda@gmail.com>
2021-06-29 18:26:13 -0700

    bananas forever, badgers, my ages

---
+++ _05_for_loops/_2_badgers/Badgers.py
@@ -0,0 +1,11 @@
+for i in range(12):
+    print('badger')
+
+for x in range(2):
+    print('mushroom')
+
+for y in range(12):
+    print('badger')
+
+for d in range(2):
+    print("mushroom")
--- _05_for_loops/_3_bananas_forever/bananas_forever.py
+++ _05_for_loops/_3_bananas_forever/bananas_forever.py
@@ -1,4 +1,7 @@
 """
  * Write a python program that prints the word 'banana' one thousand (1,000) times
 """
+for x in range(1000):
+    print('banana')
+
 
